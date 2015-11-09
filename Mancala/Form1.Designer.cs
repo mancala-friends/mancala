4558f7e3e3f0978d65cc23d9362b31acef4a74e8
zcase <zachcase.case33@gmail.com>
2015-11-08 21:17:14 -0700

    Added events to the information button. Made it so clicking exit button closes application. Made it so clicking twoPlayerButton sends to gameTab screen. Made it so application starts at the menuTab Screen.

--- Mancala/Form1.Designer.cs
+++ Mancala/Form1.Designer.cs
@@ -37,13 +37,13 @@
             this.backButton = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuTab = new System.Windows.Forms.TabPage();
+            this.titleLabel = new System.Windows.Forms.Label();
             this.exitButton = new System.Windows.Forms.Button();
             this.aboutButton = new System.Windows.Forms.Button();
             this.instructionButton = new System.Windows.Forms.Button();
             this.networkButton = new System.Windows.Forms.Button();
             this.twoPlayerButton = new System.Windows.Forms.Button();
             this.onePlayerButton = new System.Windows.Forms.Button();
-            this.titleLabel = new System.Windows.Forms.Label();
             this.mancalaTabs.SuspendLayout();
             this.gameTab.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -88,6 +88,7 @@
             this.infoButton.Size = new System.Drawing.Size(95, 100);
             this.infoButton.TabIndex = 5;
             this.infoButton.UseVisualStyleBackColor = true;
+            this.infoButton.Click += new System.EventHandler(this.infoButton_Click);
             // 
             // backButton
             // 
@@ -97,6 +98,7 @@
             this.backButton.Size = new System.Drawing.Size(161, 100);
             this.backButton.TabIndex = 4;
             this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
             // pictureBox1
             // 
@@ -125,6 +127,16 @@
             this.menuTab.Text = "Menu";
             this.menuTab.UseVisualStyleBackColor = true;
             // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(146, 3);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(485, 55);
+            this.titleLabel.TabIndex = 6;
+            this.titleLabel.Text = "Welcome to Mancala!";
+            // 
             // exitButton
             // 
             this.exitButton.Location = new System.Drawing.Point(61, 337);
@@ -133,6 +145,7 @@
             this.exitButton.TabIndex = 5;
             this.exitButton.Text = "Exit Game";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // aboutButton
             // 
@@ -164,12 +177,13 @@
             // 
             // twoPlayerButton
             // 
-            this.twoPlayerButton.Location = new System.Drawing.Point(61, 111);
+            this.twoPlayerButton.Location = new System.Drawing.Point(61, 122);
             this.twoPlayerButton.Name = "twoPlayerButton";
             this.twoPlayerButton.Size = new System.Drawing.Size(184, 23);
             this.twoPlayerButton.TabIndex = 1;
             this.twoPlayerButton.Text = "2 Player Game";
             this.twoPlayerButton.UseVisualStyleBackColor = true;
+            this.twoPlayerButton.Click += new System.EventHandler(this.twoPlayerButton_Click);
             // 
             // onePlayerButton
             // 
@@ -180,16 +194,6 @@
             this.onePlayerButton.Text = "1 Player Game";
             this.onePlayerButton.UseVisualStyleBackColor = true;
             this.onePlayerButton.Click += new System.EventHandler(this.onePlayerButton_Click);
-            // 
-            // titleLabel
-            // 
-            this.titleLabel.AutoSize = true;
-            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.Location = new System.Drawing.Point(146, 3);
-            this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(485, 55);
-            this.titleLabel.TabIndex = 6;
-            this.titleLabel.Text = "Welcome to Mancala!";
             // 
             // Form1
             // 
