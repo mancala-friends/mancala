1d53d0b08d29f9efed78d7793245e1b62546a6fe
PikkuLy <komeaolsen@gmail.com>
2015-11-11 21:01:47 -0700

    made awesome sauce logic stuff

---
+++ rules.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class rules
+{
+    private gameState gamestate;
+
+    public bool tryMove(int pit);
+
+    public gameState getGamestate() { return gamestate; }
+
+    
+	public rules()
+	{
+        gamestate = new gameState();
+	}
+}
