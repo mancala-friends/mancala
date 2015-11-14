bfd73eea014a822399f26ef6cfca22b13563c3e8
PikkuLy <komeaolsen@gmail.com>
2015-11-14 13:32:30 -0700

    did the hard parts... aka moving and checking state. Just needs some tests :)

--- TestMancala/UnitTest1.cs
+++ TestMancala/UnitTest1.cs
@@ -1,6 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+namespace Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void tryMoveTest()
+        {
+
+
+            Assert.Fail();
+        }
+    }
+}
+
 namespace TestMancala
 {
     [TestClass]
